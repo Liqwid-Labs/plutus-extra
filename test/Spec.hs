469a69dec613eba78346239479af6ad72bcd28d1
gege251 <gege251@gmail.com>
2021-08-20 11:05:00 +0900

    Add test for NonEmpty

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,6 +12,7 @@ import Suites.Plutus.PAB.CurrencyForge qualified as CurrencyForge
 import Suites.PlutusTx.Bimap qualified as Bimap
 import Suites.PlutusTx.Data.Extra qualified as DataExtra
 import Suites.PlutusTx.Set qualified as Set
+import Suites.PlutusTx.NonEmpty qualified as NonEmpty
 
 --------------------------------------------------------------------------------
 
@@ -22,6 +23,7 @@ main =
       "Suites"
       [ testGroup "PlutusTx.Bimap" Bimap.tests
       , testGroup "PlutusTx.Set" Set.tests
+      , testGroup "PlutusTx.NonEmpty" NonEmpty.tests
       , testGroup "PlutusTx.Data.Extra" DataExtra.tests
       , testGroup "Plutus.PAB.CurrencyForge" CurrencyForge.tests
       ]
