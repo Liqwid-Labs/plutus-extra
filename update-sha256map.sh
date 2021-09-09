dd621146ba3c30f32b62a939a2fff315babbf7ed
Kirill Boltaev <aske@fmap.me>
2021-09-09 20:07:50 +0300

    Run jq via nix run

--- update-sha256map.sh
+++ update-sha256map.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nix-instantiate nix/update.nix --eval --strict --json | jq -r '.[] | "nix run nixpkgs.niv -c niv update \(.name) -r \(.tag)"' | bash -x
+nix-instantiate nix/update.nix --eval --strict --json | nix run nixpkgs.jq -c jq -r '.[] | "nix run nixpkgs.niv -c niv update \(.name) -r \(.tag)"' | bash -x
