8b83a08772dd6482b24e3898b81385da39d4a8e2
Koz Ross <koz.ross@retro-freedom.nz>
2021-09-23 11:14:12 +1200

    Revert to using Haskell instead of Plutus

--- tasty-plutus/src/Test/Tasty/Plutus/Validator/Unit.hs
+++ tasty-plutus/src/Test/Tasty/Plutus/Validator/Unit.hs
@@ -13,10 +13,6 @@ module Test.Tasty.Plutus.Validator.Unit (
   TestData (..),
   WithValidator,
 
-  -- * Wrappers
-  toTestValidator,
-  toTestMintingPolicy,
-
   -- * Testing API
   withValidator,
   shouldValidate,
@@ -30,32 +26,13 @@ import Data.Kind (Type)
 import Data.Sequence (Seq)
 import Data.Sequence qualified as Seq
 import Data.Tagged (Tagged (Tagged))
-import Data.Text (Text)
-import Data.Text qualified as Text
 import GHC.Exts (toList)
 import Ledger.Value (Value)
 import Plutus.V1.Ledger.Contexts (ScriptContext)
-import Plutus.V1.Ledger.Scripts (
-  Context (Context),
-  Datum (Datum),
-  MintingPolicy,
-  Redeemer (Redeemer),
-  ScriptError,
-  Validator,
-  runMintingPolicyScript,
-  runScript,
- )
-import PlutusTx.Builtins (
-  BuiltinData,
-  BuiltinString,
-  appendString,
-  trace,
- )
 import PlutusTx.IsData.Class (
-  FromData (fromBuiltinData),
-  ToData (toBuiltinData),
+  FromData,
+  ToData,
  )
-import Safe (lastMay)
 import Test.Tasty (testGroup)
 import Test.Tasty.Plutus.Context.Internal (
   ContextBuilder,
@@ -75,52 +52,13 @@ import Test.Tasty.Providers (
 import Text.PrettyPrint (
   Doc,
   Style (lineLength),
-  colon,
   hang,
-  int,
   renderStyle,
   style,
-  text,
-  vcat,
   ($+$),
-  (<+>),
  )
 import Text.Show.Pretty (ppDoc)
 import Type.Reflection (Typeable)
-
--- | @since 3.0
-{-# INLINEABLE toTestValidator #-}
-toTestValidator ::
-  forall (datum :: Type) (redeemer :: Type).
-  (FromData datum, FromData redeemer) =>
-  (datum -> redeemer -> ScriptContext -> Bool) ->
-  (BuiltinData -> BuiltinData -> BuiltinData -> ())
-toTestValidator f d r p = case fromBuiltinData d of
-  Nothing -> reportParseFailed "Datum"
-  Just d' -> case fromBuiltinData r of
-    Nothing -> reportParseFailed "Redeemer"
-    Just r' -> case fromBuiltinData p of
-      Nothing -> reportParseFailed "ScriptContext"
-      Just p' ->
-        if f d' r' p'
-          then reportPass
-          else reportFail
-
--- | @since 3.0
-{-# INLINEABLE toTestMintingPolicy #-}
-toTestMintingPolicy ::
-  forall (redeemer :: Type).
-  (FromData redeemer) =>
-  (redeemer -> ScriptContext -> Bool) ->
-  (BuiltinData -> BuiltinData -> ())
-toTestMintingPolicy f r p = case fromBuiltinData r of
-  Nothing -> reportParseFailed "Redeemer"
-  Just r' -> case fromBuiltinData p of
-    Nothing -> reportParseFailed "ScriptContext"
-    Just p' ->
-      if f r' p'
-        then reportPass
-        else reportFail
 
 {- | A structure housing a validator, and the data needed to execute it.
 
@@ -136,7 +74,7 @@ data TestData (p :: Purpose) where
     , Show datum
     , Show redeemer
     ) =>
-    Validator ->
+    (datum -> redeemer -> ScriptContext -> Bool) ->
     datum ->
     redeemer ->
     Value ->
@@ -144,7 +82,7 @@ data TestData (p :: Purpose) where
   -- | @since 3.0
   MintingTest ::
     (ToData redeemer, FromData redeemer, Show redeemer) =>
-    MintingPolicy ->
+    (redeemer -> ScriptContext -> Bool) ->
     redeemer ->
     TestData 'ForMinting
 
@@ -223,44 +161,28 @@ instance (Typeable p) => IsTest (ValidatorTest p) where
   run _ (ValidatorTest expected cb (conf, td)) _ = pure $ case td of
     SpendingTest validator d r val ->
       let context = compileSpending conf cb d val
-          context' = Context . toBuiltinData $ context
-          d' = Datum . toBuiltinData $ d
-          r' = Redeemer . toBuiltinData $ r
-       in case runScript context' validator d' r' of
-            Left err -> testFailed . formatScriptError $ err
-            Right (_, logs) -> deliverResult expected logs conf context td
+          actual = validator d r context
+       in deliverResult expected actual conf context td
     MintingTest mp r ->
       let context = compileMinting conf cb
-          context' = Context . toBuiltinData $ context
-          r' = Redeemer . toBuiltinData $ r
-       in case runMintingPolicyScript context' mp r' of
-            Left err -> testFailed . formatScriptError $ err
-            Right (_, logs) -> deliverResult expected logs conf context td
+          actual = mp r context
+       in deliverResult expected actual conf context td
   testOptions = Tagged []
 
 deliverResult ::
   forall (p :: Purpose).
   Outcome ->
-  [Text] ->
+  Bool ->
   TransactionConfig ->
   ScriptContext ->
   TestData p ->
   Result
-deliverResult expected logs conf sc td =
-  case (expected, lastMay logs >>= Text.stripPrefix "tasty-plutus: ") of
-    (_, Nothing) -> testFailed noOutcome
-    (Fail, Just "Pass") -> testFailed unexpectedSuccess
-    (Fail, Just "Fail") -> testPassed ""
-    (Pass, Just "Pass") -> testPassed ""
-    (Pass, Just "Fail") -> testFailed unexpectedFailure
-    (_, Just t) -> case Text.stripPrefix "Parse failed: " t of
-      Nothing -> testFailed . internalError $ t
-      Just t' -> testFailed . noParse $ t'
+deliverResult expected actual conf sc td = case (expected, actual) of
+  (Fail, False) -> testPassed ""
+  (Fail, True) -> testFailed unexpectedSuccess
+  (Pass, False) -> testFailed unexpectedFailure
+  (Pass, True) -> testPassed ""
   where
-    noOutcome :: String
-    noOutcome =
-      renderStyle ourStyle $
-        "No outcome from run" $+$ dumpState
     unexpectedSuccess :: String
     unexpectedSuccess =
       renderStyle ourStyle $
@@ -269,21 +191,12 @@ deliverResult expected logs conf sc td =
     unexpectedFailure =
       renderStyle ourStyle $
         "Unexpected failure" $+$ dumpState
-    internalError :: Text -> String
-    internalError msg =
-      renderStyle ourStyle $
-        ("Internal error" <+> (text . show $ msg)) $+$ dumpState
-    noParse :: Text -> String
-    noParse what =
-      renderStyle ourStyle $
-        ((text . show $ what) <+> "did not parse") $+$ dumpState
     dumpState :: Doc
     dumpState =
       ""
         $+$ hang "Context" 4 (ppDoc sc)
         $+$ hang "Configuration" 4 (ppDoc conf)
         $+$ hang "Inputs" 4 dumpInputs
-        $+$ hang "Logs" 4 dumpLogs
     dumpInputs :: Doc
     dumpInputs = case td of
       SpendingTest _ d r v ->
@@ -295,30 +208,6 @@ deliverResult expected logs conf sc td =
           $+$ ppDoc v
       MintingTest _ r ->
         "Redeemer" $+$ ppDoc r
-    dumpLogs :: Doc
-    dumpLogs = vcat . fmap go . zip [1 ..] $ logs
-    go :: (Int, Text) -> Doc
-    go (ix, line) = (int ix <> colon) <+> (text . show $ line)
 
 ourStyle :: Style
 ourStyle = style {lineLength = 80}
-
-formatScriptError :: ScriptError -> String
-formatScriptError =
-  renderStyle ourStyle . hang "Script execution error:" 4 . ppDoc
-
-{-# INLINEABLE reportParseFailed #-}
-reportParseFailed :: BuiltinString -> ()
-reportParseFailed what = report ("Parse failed: " `appendString` what)
-
-{-# INLINEABLE reportPass #-}
-reportPass :: ()
-reportPass = report "Pass"
-
-{-# INLINEABLE reportFail #-}
-reportFail :: ()
-reportFail = report "Fail"
-
-{-# INLINEABLE report #-}
-report :: BuiltinString -> ()
-report what = trace ("tasty-plutus: " `appendString` what) ()
