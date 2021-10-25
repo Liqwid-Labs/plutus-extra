60c037e6c2718778e0220efb474c4a8f9be557fa
Koz Ross <koz.ross@retro-freedom.nz>
2021-10-26 11:19:25 +1300

    Stability, seed and length checks

--- plutus-golden/src/Test/Tasty/Plutus/Golden.hs
+++ plutus-golden/src/Test/Tasty/Plutus/Golden.hs
@@ -6,8 +6,6 @@ module Test.Tasty.Plutus.Golden (
   goldenJSON,
   goldenData,
   goldenUnsafeData,
-  goldenToSchema,
-  goldenToArgument,
 
   -- * Options
   GoldenSeed (..),
@@ -21,7 +19,6 @@ import Data.Kind (Type)
 import Data.Proxy (Proxy (Proxy))
 import Data.Tagged (Tagged (Tagged))
 import PlutusTx.IsData.Class (FromData, ToData, UnsafeFromData)
-import Schema qualified as Plutus
 import System.Random.Stateful (mkStdGen, newIOGenM)
 import Test.Tasty (TestTree)
 import Test.Tasty.Options (
@@ -36,7 +33,7 @@ import Test.Tasty.Options (
   lookupOption,
  )
 import Test.Tasty.Plutus.Generator (Generator)
-import Test.Tasty.Plutus.Golden.Config (Config (Config))
+import Test.Tasty.Plutus.Golden.Internal (Config (Config))
 import Test.Tasty.Plutus.Golden.JSON (doGoldenJSON)
 import Test.Tasty.Providers (
   IsTest (run, testOptions),
@@ -78,28 +75,6 @@ goldenUnsafeData ::
   TestTree
 goldenUnsafeData =
   singleTest ("Golden UnsafeData: " <> tyName) . GoldenUnsafeData tyName
-  where
-    tyName :: String
-    tyName = typeName @a
-
-goldenToSchema ::
-  forall (a :: Type).
-  (Typeable a, Plutus.ToSchema a) =>
-  Generator a ->
-  TestTree
-goldenToSchema =
-  singleTest ("Golden ToSchema: " <> tyName) . GoldenToSchema tyName
-  where
-    tyName :: String
-    tyName = typeName @a
-
-goldenToArgument ::
-  forall (a :: Type).
-  (Typeable a, Plutus.ToArgument a) =>
-  Generator a ->
-  TestTree
-goldenToArgument =
-  singleTest ("Golden ToArgument: " <> tyName) . GoldenToArgument tyName
   where
     tyName :: String
     tyName = typeName @a
@@ -194,15 +169,13 @@ data GoldenTest (a :: Type) where
   GoldenJSON :: (ToJSON a, FromJSON a) => String -> Generator a -> GoldenTest a
   GoldenData :: (FromData a, ToData a) => String -> Generator a -> GoldenTest a
   GoldenUnsafeData :: (UnsafeFromData a, ToData a) => String -> Generator a -> GoldenTest a
-  GoldenToSchema :: (Plutus.ToSchema a) => String -> Generator a -> GoldenTest a
-  GoldenToArgument :: (Plutus.ToArgument a) => String -> Generator a -> GoldenTest a
 
 instance (Typeable a) => IsTest (GoldenTest a) where
   run opts gt _ = do
     rng <- newIOGenM . mkStdGen $ seed
     case gt of
       GoldenJSON tyName gen -> do
-        let conf = Config tyName seed rng goldenPath sampleSize
+        let conf = Config tyName seed rng gen goldenPath sampleSize
         runReaderT doGoldenJSON conf
       _ -> pure . testFailed $ "Unsupported for now"
     where
@@ -212,15 +185,6 @@ instance (Typeable a) => IsTest (GoldenTest a) where
       GoldenPath goldenPath = lookupOption opts
       sampleSize :: Int
       GoldenSampleSize sampleSize = lookupOption opts
-
-  {-
-    case gt of
-      GoldenJSON tyName gen -> _ -- doGoldenJSON tyName gen
-      GoldenData _ _ -> pure . testFailed $ "Unsupported for now"
-      GoldenUnsafeData _ _ -> pure . testFailed $ "Unsupported for now"
-      GoldenToSchema _ _ -> pure . testFailed $ "Unsupported for now"
-      GoldenToArgument _ _ -> pure . testFailed $ "Unsupported for now"
-  -}
   testOptions =
     Tagged
       [ Option (Proxy @GoldenSeed)
--- plutus-golden/src/Test/Tasty/Plutus/Golden/JSON.hs
+++ plutus-golden/src/Test/Tasty/Plutus/Golden/JSON.hs
@@ -2,38 +2,24 @@
 
 module Test.Tasty.Plutus.Golden.JSON (doGoldenJSON) where
 
-import Control.Monad.Extra (ifM, unlessM)
+import Control.Monad.Extra (ifM)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Reader (ReaderT, asks)
-import Data.Aeson (
-  ToJSON (toJSON),
-  Value,
-  eitherDecodeFileStrict',
- )
+import Data.Aeson (ToJSON (toJSON), Value)
 import Data.Aeson.Encode.Pretty (encodePretty)
-import Data.ByteString qualified as BS
 import Data.ByteString.Lazy qualified as Lazy
 import Data.Function (on)
 import Data.Kind (Type)
 import Data.Maybe (mapMaybe)
-import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as Encoding
-import Data.Vector (Vector)
 import Data.Vector qualified as Vector
 import System.Directory (
   createDirectoryIfMissing,
-  doesDirectoryExist,
   doesFileExist,
   getCurrentDirectory,
  )
 import System.FilePath ((<.>), (</>))
-import System.Random.Stateful (
-  IOGenM,
-  StdGen,
-  mkStdGen,
-  newIOGenM,
- )
 import Test.Tasty.Plutus.Generator (Generator (Generator))
 import Test.Tasty.Plutus.Golden.Internal (
   Config (
@@ -74,6 +60,7 @@ import Type.Reflection (
   typeRep,
   typeRepTyCon,
  )
+import Prelude hiding (exp)
 
 doGoldenJSON ::
   forall (a :: Type).
@@ -126,36 +113,95 @@ loadAndCompareSamples ::
   ReaderT (Config a) IO Result
 loadAndCompareSamples fp sample = do
   result <- liftIO . deserializeSample pure $ fp
-  case result of
-    Left err -> pure . testFailed . sampleError $ err
+  pure $ case result of
+    Left err -> testFailed . sampleError $ err
     Right expected -> compareSamples fp expected sample
+  where
+    sampleError :: SampleError -> String
+    sampleError err = renderStyle ourStyle $ case err of
+      NotJSON err' ->
+        "Could not parse sample file as JSON."
+          $+$ ""
+          $+$ dumpFileLocation fp
+          $+$ ""
+          $+$ "Parse error"
+          $+$ ""
+          $+$ text err'
+      NotSample err' ->
+        "Given file is not a valid sample file."
+          $+$ ""
+          $+$ dumpFileLocation fp
+          $+$ ""
+          $+$ "Parse error"
+          $+$ ""
+          $+$ text err'
 
 compareSamples ::
-  forall (a :: Type).
   FilePath ->
   Sample Value ->
   Sample Value ->
-  ReaderT (Config a) IO Result
+  Result
 compareSamples fp expected actual =
   case (compare `on` sampleSeed) expected actual of
     EQ -> case (compare `on` (Vector.length . sampleData)) expected actual of
-      EQ -> do
+      EQ ->
         let expVec = sampleData expected
-        let actVec = sampleData actual
-        Vector.ifoldM (go fp) (testPassed "") . Vector.zip expVec $ actVec
-      _ -> _
-    _ -> _
+            actVec = sampleData actual
+         in Vector.ifoldl' go (testPassed "") . Vector.zip expVec $ actVec
+      _ -> testFailed mismatchedSampleCounts
+    _ -> testFailed mismatchedSeeds
   where
     go ::
-      FilePath ->
       Result ->
       Int ->
       (Value, Value) ->
-      ReaderT (Config a) IO Result
-    go fp acc ix (ex, act) = _
+      Result
+    go acc ix (ex, act)
+      | resultSuccessful acc = case compare ex act of
+        EQ -> acc
+        _ -> testFailed . mismatchedSample ix ex $ act
+      | otherwise = acc
+    mismatchedSampleCounts :: String
+    mismatchedSampleCounts =
+      let expectedLen = Vector.length . sampleData $ expected
+          actualLen = Vector.length . sampleData $ actual
+       in renderStyle ourStyle $
+            "Unexpected number of samples"
+              $+$ ""
+              $+$ dumpFileLocation fp
+              $+$ "Expected:" <+> int expectedLen
+              $+$ "Actual:" <+> int actualLen
+    mismatchedSeeds :: String
+    mismatchedSeeds =
+      let expectedSeed = sampleSeed expected
+          actualSeed = sampleSeed actual
+       in renderStyle ourStyle $
+            "Seeds do not match"
+              $+$ ""
+              $+$ dumpFileLocation fp
+              $+$ "Expected:" <+> int expectedSeed
+              $+$ "Actual:" <+> int actualSeed
+    mismatchedSample ::
+      Int ->
+      Value ->
+      Value ->
+      String
+    mismatchedSample ix exp act =
+      renderStyle ourStyle $
+        "Sample" <+> int ix <+> "does not match."
+          $+$ ""
+          $+$ dumpFileLocation fp
+          $+$ ""
+          $+$ "Expected"
+          $+$ ""
+          $+$ renderJSON exp
+          $+$ ""
+          $+$ "Actual"
+          $+$ ""
+          $+$ renderJSON act
 
-sampleError :: SampleError -> String
-sampleError err = renderStyle ourStyle _
+dumpFileLocation :: FilePath -> Doc
+dumpFileLocation fp = "Sample file location:" <+> text fp
 
 writeSampleToFile ::
   FilePath ->
@@ -169,97 +215,10 @@ writeSampleToFile fp sample = do
 ourStyle :: Style
 ourStyle = style {lineLength = 80}
 
-{-
-doGoldenJSON ::
-  forall (a :: Type).
-  (Typeable a, ToJSON a) =>
-  String ->
-  Generator a ->
-  IO Result
-doGoldenJSON tyName (Generator f) = do
-  rng <- newIOGenM . mkStdGen $ 123456
-  cwd <- getCurrentDirectory
-  let goldenPath = cwd </> "golden" </> "json"
-  let goldenFilePath = goldenPath </> sampleFileName @a tyName <.> "json"
-  unlessM (doesDirectoryExist goldenPath) (createDirectoryIfMissing True goldenPath)
-  sampleArray <- genSamples rng
-  ifM
-    (doesFileExist goldenFilePath)
-    (loadAndCompareSamples goldenFilePath sampleArray)
-    (writeSamplesToFile goldenFilePath sampleArray)
-  where
-    genSamples :: IOGenM StdGen -> IO (Vector Value)
-    genSamples rng = Vector.replicateM numSamples (toJSON <$> f rng)
-
-sampleFileName :: forall (a :: Type). (Typeable a) => String -> FilePath
-sampleFileName tyName = mapMaybe go $ moduleName <> "." <> tyName
-  where
-    go :: Char -> Maybe Char
-    go =
-      pure . \case
-        '.' -> '-'
-        c -> c
-    moduleName :: String
-    moduleName = tyConModule . typeRepTyCon $ typeRep @a
-
-writeSamplesToFile :: FilePath -> Vector Value -> IO Result
-writeSamplesToFile p vals = do
-  BS.writeFile p . Lazy.toStrict . encodePretty $ vals
-  pure . testPassed . renderStyle ourStyle $
-    "Generated sample file:" <+> text p
-
-loadAndCompareSamples :: FilePath -> Vector Value -> IO Result
-loadAndCompareSamples fp vals = do
-  result <- eitherDecodeFileStrict' fp
-  pure $ case result of
-    Left err -> testFailed . parseError $ err
-    Right sampleVals ->
-      Vector.ifoldl' go (testPassed "") . Vector.zip sampleVals $ vals
-  where
-    go :: Result -> Int -> (Value, Value) -> Result
-    go acc ix (expected, actual)
-      | resultSuccessful acc = case compare expected actual of
-        EQ -> acc
-        _ -> testFailed . mismatch ix expected $ actual
-      | otherwise = acc
-    parseError :: String -> String
-    parseError err =
-      renderStyle ourStyle $
-        "Not a valid sample file."
-          $+$ "Either this file wasn't JSON, or it wasn't a JSON array."
-          $+$ ""
-          $+$ dumpFileLocation
-          $+$ ""
-          $+$ "Error"
-          $+$ ""
-          $+$ text err
-    mismatch :: Int -> Value -> Value -> String
-    mismatch ix expected actual =
-      renderStyle ourStyle $
-        "Sample" <+> int ix <+> "did not match."
-          $+$ ""
-          $+$ dumpFileLocation
-          $+$ ""
-          $+$ "Expected"
-          $+$ ""
-          $+$ jsonToDoc expected
-          $+$ ""
-          $+$ "Actual"
-          $+$ ""
-          $+$ jsonToDoc actual
-    dumpFileLocation :: Doc
-    dumpFileLocation =
-      "Sample file location"
-        $+$ ""
-        $+$ text fp
-
-jsonToDoc :: Value -> Doc
-jsonToDoc = text . T.unpack . renderJSON
-
-renderJSON :: Value -> Text
-renderJSON = Encoding.decodeUtf8 . Lazy.toStrict . encodePretty
-
-numSamples :: Int
-numSamples = 20
-
--}
+renderJSON :: Value -> Doc
+renderJSON =
+  text
+    . T.unpack
+    . Encoding.decodeUtf8
+    . Lazy.toStrict
+    . encodePretty
