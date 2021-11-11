e5fa954216f0debff2df8a5d650fa6da61903b4a
Koz Ross <koz.ross@retro-freedom.nz>
2021-11-11 13:12:14 +1300

    Function instance for BuiltinByteString

--- quickcheck-plutus-instances/src/Test/QuickCheck/Plutus/Instances.hs
+++ quickcheck-plutus-instances/src/Test/QuickCheck/Plutus/Instances.hs
@@ -2,24 +2,28 @@
 
 module Test.QuickCheck.Plutus.Instances () where
 
-import PlutusTx (Data)
+import Data.Word (Word8)
 import GHC.Exts qualified as GHC
 import PlutusTx.Prelude qualified as PTx
-import PlutusTx.Builtins.Internal qualified as Internal
+import PlutusTx.Builtins.Internal (BuiltinByteString (BuiltinByteString))
 import Test.QuickCheck.Arbitrary (Arbitrary (arbitrary, shrink))
+import Test.QuickCheck.Function (Function (function), functionMap)
 import Test.QuickCheck.Gen (Gen)
 import Data.ByteString (ByteString)
 
 -- | @since 1.0
 instance Arbitrary PTx.BuiltinByteString where
-  arbitrary = Internal.BuiltinByteString <$> genByteString
-  shrink (Internal.BuiltinByteString bs) = 
-    Internal.BuiltinByteString <$> shrinkByteString bs
+  arbitrary = BuiltinByteString <$> genByteString
+  shrink (BuiltinByteString bs) = BuiltinByteString <$> shrinkByteString bs
 
 -- | @since 1.0
-instance Arbitrary Data where
-  arbitrary = _
-  shrink = _
+instance Function PTx.BuiltinByteString where
+  function = functionMap intoByteList outOfByteList
+    where
+      intoByteList :: BuiltinByteString -> [Word8]
+      intoByteList (BuiltinByteString bs) = GHC.toList bs
+      outOfByteList :: [Word8] -> BuiltinByteString
+      outOfByteList = BuiltinByteString . GHC.fromList
 
 -- Helpers
 
