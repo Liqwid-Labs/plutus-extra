7dc35ea00e4aa252879386efe876b2dfe095c98d
Koz Ross <koz.ross@retro-freedom.nz>
2021-12-16 09:01:42 +1300

    Tests for sizing

--- plutus-numeric/test/sizing-proof/Functions.hs
+++ plutus-numeric/test/sizing-proof/Functions.hs
@@ -5,10 +5,14 @@ module Functions (
   round1,
   round2,
   round3,
-  round4
-  ) where
+  round4,
+  fOurs,
+  fTheirs,
+) where
 
 import PlutusTx.Prelude
+import PlutusTx.Ratio qualified as Theirs
+import PlutusTx.Rational qualified as Ours
 
 {-# INLINEABLE round1 #-}
 round1 :: [Rational] -> [Integer]
@@ -25,3 +29,17 @@ round3 (x1 : x2 : x3 : _ : _) = [round x1, round x2, round x3, 1]
 {-# INLINEABLE round4 #-}
 round4 :: [Rational] -> [Integer]
 round4 (x1 : x2 : x3 : x4 : _) = [round x1, round x2, round x3, round x4]
+
+{-# INLINEABLE fOurs #-}
+fOurs :: Ours.Rational -> Ours.Rational -> Integer
+fOurs x y = Ours.round . Ours.negate $ cube (Ours.abs x) + cube (Ours.abs y)
+  where
+    cube :: Ours.Rational -> Ours.Rational
+    cube z = z * z * z
+
+{-# INLINEABLE fTheirs #-}
+fTheirs :: Rational -> Rational -> Integer
+fTheirs x y = round . negate $ cube (Theirs.abs x) + cube (Theirs.abs y)
+  where
+    cube :: Rational -> Rational
+    cube z = z * z * z
--- plutus-numeric/test/sizing-proof/Main.hs
+++ plutus-numeric/test/sizing-proof/Main.hs
@@ -2,36 +2,55 @@
 
 module Main (main) where
 
-import PlutusTx.Code (CompiledCode)
-import Test.Tasty (defaultMain, testGroup)
-import Test.Tasty.Plutus.Size (fitsOnChain)
+import Functions (
+  fOurs,
+  fTheirs,
+  round1,
+  round2,
+  round3,
+  round4,
+ )
 import Plutus.V1.Ledger.Scripts (fromCompiledCode)
+import PlutusTx.Code (CompiledCode)
 import PlutusTx.Prelude qualified as Plutus
+import PlutusTx.Rational qualified as Our
 import PlutusTx.TH (compile)
-import Functions (round1, round2, round3, round4)
+import Test.Tasty (defaultMain, testGroup)
+import Test.Tasty.Plutus.Size (fitsOnChain, fitsUnder)
 
 main :: IO ()
-main = defaultMain . testGroup "Demo" $ [
-  fitsOnChain "round" . fromCompiledCode $ roundCode,
-  fitsOnChain "round1" . fromCompiledCode $ round1Code,
-  fitsOnChain "round2" . fromCompiledCode $ round2Code,
-  fitsOnChain "round3" . fromCompiledCode $ round3Code,
-  fitsOnChain "round4" . fromCompiledCode $ round4Code
-  ]
+main =
+  defaultMain . testGroup "Demo" $
+    [ fitsOnChain "round" . fromCompiledCode $ roundCode
+    , fitsOnChain "round1" . fromCompiledCode $ round1Code
+    , fitsOnChain "round2" . fromCompiledCode $ round2Code
+    , fitsOnChain "round3" . fromCompiledCode $ round3Code
+    , fitsOnChain "round4" . fromCompiledCode $ round4Code
+    , fitsUnder
+        "round negated sum of absolute cubes (ours versus Plutus)"
+        (fromCompiledCode ours)
+        (fromCompiledCode theirs)
+    ]
 
 -- Helpers
 
 roundCode :: CompiledCode (Plutus.Rational -> Plutus.Integer)
-roundCode = $$(compile [|| Plutus.round ||])
+roundCode = $$(compile [||Plutus.round||])
 
 round1Code :: CompiledCode ([Plutus.Rational] -> [Plutus.Integer])
-round1Code = $$(compile [|| round1 ||])
+round1Code = $$(compile [||round1||])
 
 round2Code :: CompiledCode ([Plutus.Rational] -> [Plutus.Integer])
-round2Code = $$(compile [|| round2 ||])
+round2Code = $$(compile [||round2||])
 
 round3Code :: CompiledCode ([Plutus.Rational] -> [Plutus.Integer])
-round3Code = $$(compile [|| round3 ||])
+round3Code = $$(compile [||round3||])
 
 round4Code :: CompiledCode ([Plutus.Rational] -> [Plutus.Integer])
-round4Code = $$(compile [|| round4 ||])
+round4Code = $$(compile [||round4||])
+
+ours :: CompiledCode (Our.Rational -> Our.Rational -> Plutus.Integer)
+ours = $$(compile [||fOurs||])
+
+theirs :: CompiledCode (Plutus.Rational -> Plutus.Rational -> Plutus.Integer)
+theirs = $$(compile [||fTheirs||])
