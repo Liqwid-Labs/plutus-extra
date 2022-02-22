9ee82756f6acb0f8911d23dffe10926f84891bc2
gnumonik <gnumonik@protonmail.com>
2022-02-22 07:43:44 +0000

    Fixed #188: Replaced the 'fitsOnChain' size tests with 'fitsInto' tests where the maximum is the function's current script size. Refactored the test function to avoid repetition + increase readability.

--- plutus-collection/test/size/Main.hs
+++ plutus-collection/test/size/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 import Plutus.V1.Ledger.Scripts (fromCompiledCode)
 import PlutusTx (CompiledCode)
@@ -9,41 +10,51 @@ import PlutusTx.List.Ord qualified as Ord
 import PlutusTx.Natural (Natural)
 import PlutusTx.Prelude
 import PlutusTx.TH (compile)
-import Test.Tasty (defaultMain, testGroup)
-import Test.Tasty.Plutus.Size (fitsOnChain)
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.Plutus.Size (ByteSize, bytes, fitsInto)
 import Prelude qualified
+import Data.Kind (Type)
 
+-- byte values are script sizes from c36555e35289c7aaabf62af8dccac65d9ab52c11
 main :: Prelude.IO ()
 main =
   defaultMain . testGroup "Size checks" $
     [ testGroup
         "List.Natural"
-        [ fitsOnChain "length" . fromCompiledCode $ naturalLength
-        , fitsOnChain "take" . fromCompiledCode $ naturalTake
-        , fitsOnChain "drop" . fromCompiledCode $ naturalDrop
-        , fitsOnChain "splitAt" . fromCompiledCode $ naturalSplitAt
-        , fitsOnChain "replicate" . fromCompiledCode $ naturalReplicate
+        [ runSizeTest "length"    [bytes| 121 |] naturalLength
+        , runSizeTest "take"      [bytes| 173 |] naturalTake
+        , runSizeTest "drop"      [bytes| 168 |] naturalDrop
+        , runSizeTest "splitAt"   [bytes| 263 |] naturalSplitAt
+        , runSizeTest "replicate" [bytes| 163 |] naturalReplicate
         ]
     , testGroup
         "List.Ord"
-        [ fitsOnChain "sort" . fromCompiledCode $ ordSort
-        , fitsOnChain "sortOn" . fromCompiledCode $ ordSortOn
-        , fitsOnChain "sortBy" . fromCompiledCode $ ordSortBy
-        , fitsOnChain "ordNub" . fromCompiledCode $ ordOrdNub
-        , fitsOnChain "ordNubBy" . fromCompiledCode $ ordOrdNubBy
-        , fitsOnChain "isSorted" . fromCompiledCode $ ordIsSorted
-        , fitsOnChain "isSortedOn" . fromCompiledCode $ ordIsSortedOn
-        , fitsOnChain "isSortedAscending" . fromCompiledCode $ ordIsSortedAscending
-        , fitsOnChain "isSortedAscendingOn" . fromCompiledCode $ ordIsSortedAscendingOn
-        , fitsOnChain "isSortedBy" . fromCompiledCode $ ordIsSortedBy
+        [ runSizeTest "sort"                [bytes| 467 |] ordSort
+        , runSizeTest "sortOn"              [bytes| 469 |] ordSortOn
+        , runSizeTest "sortBy"              [bytes| 266 |] ordSortBy
+        , runSizeTest "ordNub"              [bytes| 540 |] ordOrdNub
+        , runSizeTest "ordNubBy"            [bytes| 340 |] ordOrdNubBy
+        , runSizeTest "isSorted"            [bytes| 426 |] ordIsSorted
+        , runSizeTest "isSortedOn"          [bytes| 428 |] ordIsSortedOn
+        , runSizeTest "isSortedAscending"   [bytes| 426 |] ordIsSortedAscending
+        , runSizeTest "isSortedAscendingOn" [bytes| 428 |] ordIsSortedAscendingOn
+        , runSizeTest "isSortedBy"          [bytes| 218 |] ordIsSortedBy
         ]
     , testGroup
         "AssocMap.Natural"
-        [ fitsOnChain "take" . fromCompiledCode $ naturalMapTake
-        , fitsOnChain "drop" . fromCompiledCode $ naturalMapDrop
-        , fitsOnChain "splitAt" . fromCompiledCode $ naturalMapSplitAt
+        [ runSizeTest "take"    [bytes| 178 |] naturalMapTake
+        , runSizeTest "drop"    [bytes| 173 |] naturalMapDrop
+        , runSizeTest "splitAt" [bytes| 290 |] naturalMapSplitAt
         ]
     ]
+  where
+    runSizeTest :: forall (a :: Type)
+                 . Prelude.String 
+                -> ByteSize 
+                -> CompiledCode a 
+                -> TestTree
+    runSizeTest scriptName maxSize = 
+      fitsInto scriptName maxSize . fromCompiledCode
 
 {-# INLINEABLE naturalLength #-}
 naturalLength :: CompiledCode ([Integer] -> Natural)
